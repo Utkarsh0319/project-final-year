614900610a459280b5310eabd89c52f79a65cd27
Utkarsh0319 <99562009+Utkarsh0319@users.noreply.github.com>
2024-04-15 19:06:56 +0530

    Add files via upload

---
+++ peerfect working.py
@@ -0,0 +1,77 @@
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+import imutils
+import dlib
+import cv2
+
+mixer.init()
+mixer.music.load("music.wav")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+thresh_closed = 0.25  
+thresh_not_detected = 0.1
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+cap = cv2.VideoCapture(0)
+flag_closed = 0
+flag_not_detected = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+
+    alert_message = " ALERT  ALERT  ALERT  ALERT  ALERT  ALERT "
+    if len(subjects) == 0:
+        flag_not_detected += 1
+        if flag_not_detected >= frame_check:
+            print("Eyes not detected!")
+            mixer.music.play()
+            alert_message = "ALERT: Eyes not detected!"
+    else:
+        flag_not_detected = 0
+        for subject in subjects:
+            shape = predict(gray, subject)
+            shape = face_utils.shape_to_np(shape)
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+            if ear < thresh_closed:
+                flag_closed += 1
+                if flag_closed >= frame_check:
+                    print("Eyes closed!")
+                    mixer.music.play()
+                    alert_message = "ALERT: Eyes closed!"
+            else:
+                flag_closed = 0
+    cv2.putText(frame, alert_message, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
